c33483e20a80738890e48205f79ffc5f54790381
Imnyeong <lmh4873@gmail.com>
2024-07-26 14:49:35 +0900

    240726

---
+++ level2/올바른_괄호.cpp
@@ -0,0 +1,22 @@
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+bool solution(string s)
+{
+    int count = 0;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] == '(')
+            count++;
+        else if (s[i] == ')')
+            count--;
+
+        if (count < 0)
+            return false;
+    }
+
+    return count == 0;
+}
